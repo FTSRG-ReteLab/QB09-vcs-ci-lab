1bd044ab7a9d81c49e0dd9c529e777fe9a2390bf
Bagoly Zoltán <zoltan.bagoly@gmail.com>
2020-04-02 13:42:59 +0200

    tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,30 +1,58 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Optional;
+import org.mockito.Mock
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorTest {
 
-    private Tachograph tachograph;
+    @Mock
+    TrainController mockController;
+
+    @Mock
+    TrainUser mockTrainUser;
+
+    TrainSensor underTest;
 
     @Before
     public void before() {
-        tachograph = new Tachograph();
+        initMocks(this);
 
-        tachograph.addDataElement(1, 1, 1);
-        tachograph.addDataElement(2, 1, 15);
-        tachograph.addDataElement(1, 4, 16);
+        underTest = new TrainSensorImpl(mockController, mockTrainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        assertEquals(1, tachograph.getElement(1, 1));
-        assertEquals(16, tachograph.getElement(1, 4));
+    public void testSetAnySpeedlimit() {
+        underTest.overrideSpeedLimit(21);
+        verify(mockController).setSpeedLimit(21);
+    }
+
+    @Test
+    public void testNegativeSpeedlimit() {
+        underTest.overrideSpeedLimit(-69);
+        verify(mockTrainUser).setAlarmState(true);
+    }
+
+    @Test
+    public void testSetAeroplaneSpeed() {
+        underTest.overrideSpeedLimit(690);
+        verify(mockTrainUser).setAlarmState(true);
+    }
+
+    @Test
+    public void testSetAwfullyLowSpeedlimit() {
+        when(mockController.getReferenceSpeed()).thenReturn(200);
+        underTest.overrideSpeedLimit(99);
+
+        verify(mockTrainUser).setAlarmState(true);
     }
 }
